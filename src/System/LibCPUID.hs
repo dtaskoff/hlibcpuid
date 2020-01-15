188b4abe7a5606be5e77ce89bebe1e7cdf7d7b00
Daniel Taskoff <daniel.taskoff@gmail.com>
2020-01-15 16:54:40 +0200

    Implement a binding for checking the presence of CPUID

--- src/System/LibCPUID.hs
+++ src/System/LibCPUID.hs
@@ -1,3 +1,13 @@
 module System.LibCPUID
-  (
+  ( isCPUIDPresent
   ) where
+
+import Foreign.C.Types (CInt(..))
+
+
+-- | Check if the CPUID instruction is supported
+isCPUIDPresent :: IO Bool
+isCPUIDPresent = (== 1) <$> c_cpuid_present
+
+foreign import ccall "cpuid_present"
+  c_cpuid_present :: IO CInt
